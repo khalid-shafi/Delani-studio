d033712c47333f377bddade59b54a3c65005e536
khalid-shafi <khalidshafi2001@gmail.com>
2022-03-16 17:52:00 +0300

    added functions and eventlistner

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 $(document).ready (function(){
-    $(".HELLO").click(function(){
-        $(".bulbul").toggle();
-        $(".HIDD1").toggle();
+    $(".hello").click(function(){
+        $(".bulbul").toggle('hide');
+        $(".HIDD1").toggle('show');
 
     });
+});
+$(document).ready (function(){
+    $(".hello").click(function(){
+        $(".bulbul2").toggle('hide');
+        $(".HIDD2").toggle('show');
+
+    });
+});
+$(document).ready (function(){
+    $(".hello").click(function(){
+        $(".bulbul3").toggle('hide');
+        $(".HIDD3").toggle('show');
+
+    });
+});
+
+
+
+
+
+
+
+
+const btn = document.getElementById("btn");
+
+function formReset(){
+    document.getElementById("form").reset();
+}
+
+btn.addEventListener("click", function(event){
+    event.preventDefault();
+    let Name = document.getElementById("name").value;
+    let Email = document.getElementById("email").value;
+    let result = document.getElementById("result");
+    let error = document.getElementById("error");
+
+    if (Name === "" || Email === "") {
+       return error.innerHTML = "please enter your name and email!";
+    }else if ((Name) && (Email)){
+        result.innerHTML = Name + ", we have received your message, thank you for reaching out to us";
+    }
+    error.innerHTML = "";
+    formReset();
 });
